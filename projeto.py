bb996f72aad1c4056dd582e9315f79b5962d7f73
dionisio300 <dionisioassis80@gmail.com>
2025-05-12 21:11:52 -0300

    Projeto em equipe

---
+++ projeto.py
@@ -0,0 +1,23 @@
+# Gabriel - Cadastrar
+
+
+
+
+
+
+
+# Alexya - Listar
+
+
+
+
+clientes = [
+    {"nome":'Maria', "idade":28,"cidade":'Fortaleza'},
+    {"nome":'Ana', "idade":25,"cidade":'Caucaia'},
+    {"nome":'João', "idade":28,"cidade":'Euzebio'}
+]
+
+#Fazer um menu de opções 1 - cadastrar cliente, 2 - listar os clientes 3 - sair 
+
+
+# while True - com as opções
